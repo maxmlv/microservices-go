092efc4b016f20eafd191e1bd8a7917c3c39caca
maxmlv <max.mlv.k@gmail.com>
2022-08-27 12:18:20 +0300

    fix: no need http request

--- broker-service/cmd/api/helpers.go
+++ broker-service/cmd/api/helpers.go
@@ -8,9 +8,9 @@ import (
 )
 
 type jsonResponse struct {
-	Error   bool   `json:"error"`
-	Message string `json:"message"`
-	Data interface{} `json:"data,omitempty"` 
+	Error   bool        `json:"error"`
+	Message string      `json:"message"`
+	Data    interface{} `json:"data,omitempty"`
 }
 
 func (app *Config) readJSON(w http.ResponseWriter, r *http.Request, data interface{}) error {
@@ -54,7 +54,7 @@ func (app *Config) writeJSON(w http.ResponseWriter, status int, data interface{}
 	return nil
 }
 
-func (app *Config) errorJSON(w http.ResponseWriter, r *http.Request, err error, status ...int) error {
+func (app *Config) errorJSON(w http.ResponseWriter, err error, status ...int) error {
 	statusCode := http.StatusBadRequest
 
 	if len(status) > 0 {
